abbb16e6fe897fcea39442b401d8205dee37ba28
Nick Ishigami <nickyish13@gmail.com>
2023-04-05 19:55:26 -0400

    fix errors

--- src/main/java/edu/wpi/teamc/controllers/TableRow.java
+++ src/main/java/edu/wpi/teamc/controllers/TableRow.java
@@ -18,6 +18,7 @@ public class TableRow {
   private String nodeType;
   private String startNode;
   private String endNode;
+  private String s1, s2, s3, s4, s5, s6, s7;
 
   public TableRow() {
     this.NodeID = "";
@@ -37,6 +38,31 @@ public class TableRow {
   //    this.Date = date;
   //    this.index = index;
   //  }
+
+  public TableRow(String s1, String s2, String s3, String s4, String s5, String s6) {
+    this.s1 = s1;
+    this.s2 = s2;
+    this.s3 = s3;
+    this.s4 = s4;
+    this.s5 = s5;
+    this.s6 = s6;
+  }
+  //  public TableRow(String nodeID, String longName, String date, int index) {
+  //    this.NodeID = nodeID;
+  //    this.LongName = longName;
+  //    this.Date = date;
+  //    this.index = index;
+  //  }
+
+  public TableRow(String s1, String s2, String s3, String s4, String s5, String s6, String s7) {
+    this.s1 = s1;
+    this.s2 = s2;
+    this.s3 = s3;
+    this.s4 = s4;
+    this.s5 = s5;
+    this.s6 = s6;
+    this.s7 = s7;
+  }
 
   public TableRow(
       String nodeID,

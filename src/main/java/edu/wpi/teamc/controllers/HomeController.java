3764b1194c93de87ddb5cca0cec45f48b51694f6
azzhang3 <azhang3@wpi.edu>
2023-04-09 21:27:53 -0400

    new UI - Aaron

--- src/main/java/edu/wpi/teamc/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamc/controllers/HomeController.java
@@ -1,7 +1,5 @@
 package edu.wpi.teamc.controllers;
 
-import edu.wpi.teamc.navigation.Navigation;
-import edu.wpi.teamc.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 
@@ -10,7 +8,5 @@ public class HomeController {
   @FXML MFXButton navigateButton;
 
   @FXML
-  public void initialize() {
-    navigateButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
-  }
+  public void initialize() {}
 }

836b2429ee5d6d22545946b67ffa77cf17ace6a9
BJSamurai <juxieanchao@126.com>
2023-04-10 22:33:19 -0400

    Request pages based on Template - Meal Request

--- src/main/java/edu/wpi/teamc/controllers/ConferenceController.java
+++ src/main/java/edu/wpi/teamc/controllers/ConferenceController.java
@@ -5,31 +5,24 @@ import edu.wpi.teamc.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
 
 public class ConferenceController {
 
-    @FXML
-    private MFXButton serviceRequest;
+  @FXML private MFXButton serviceRequest;
 
+  @FXML
+  public void getService(ActionEvent event) {}
 
-    @FXML
-    public void getService(ActionEvent event) {
-    }
+  @FXML
+  public void getLogout(ActionEvent actionEvent) {
+    Navigation.navigate(Screen.HOME);
+  }
 
-    @FXML
-    public void getLogout(ActionEvent actionEvent) {
-        Navigation.navigate(Screen.HOME);
-    }
+  @FXML
+  public void getExit(ActionEvent actionEvent) {
+    System.exit(0);
+  }
 
-    @FXML
-    public void getExit(ActionEvent actionEvent) {
-        System.exit(0);
-    }
-
-    @FXML
-    public void initialize() {
-    }
-
-
+  @FXML
+  public void initialize() {}
 }

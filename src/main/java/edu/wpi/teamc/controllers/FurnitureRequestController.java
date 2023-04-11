ac0770e931434f91bf8db4cad9b04485df95fa91
BJSamurai <juxieanchao@126.com>
2023-04-10 22:34:29 -0400

    Pages slight fix #2

--- src/main/java/edu/wpi/teamc/controllers/FurnitureRequestController.java
+++ src/main/java/edu/wpi/teamc/controllers/FurnitureRequestController.java
@@ -1,4 +1,18 @@
 package edu.wpi.teamc.controllers;
 
+import edu.wpi.teamc.navigation.Navigation;
+import edu.wpi.teamc.navigation.Screen;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class FurnitureRequestController {
+    @FXML
+    public void getLogout(ActionEvent actionEvent) {
+        Navigation.navigate(Screen.HOME);
+    }
+
+    @FXML
+    public void getExit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }

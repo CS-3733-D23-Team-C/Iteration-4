2823192adf63397268d6cce3ae132e3c67e0b412
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-04-10 20:22:49 -0400

    service request not done yet

--- src/main/java/edu/wpi/teamc/dao/requests/OfficeSuppliesRequestDAO.java
+++ src/main/java/edu/wpi/teamc/dao/requests/OfficeSuppliesRequestDAO.java
@@ -1,11 +1,9 @@
 package edu.wpi.teamc.dao.requests;
 
 import edu.wpi.teamc.dao.DBConnection;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class OfficeSuppliesRequestDAO {
@@ -26,7 +24,8 @@ public class OfficeSuppliesRequestDAO {
         STATUS status = STATUS.valueOf(rs.getString("status"));
         String additionalNotes = rs.getString("additionalNotes");
         String eta = rs.getString("ETA");
-        OfficeSuppliesRequest request = new OfficeSuppliesRequest(requestID, req, roomName, supplies, additionalNotes);
+        OfficeSuppliesRequest request =
+            new OfficeSuppliesRequest(requestID, req, roomName, supplies, additionalNotes);
         request.setStatus(status);
         request.setEta(eta);
         returnList.add(request);
@@ -43,12 +42,21 @@ public class OfficeSuppliesRequestDAO {
     OfficeSuppliesRequest request = null;
     String table = "\"ServiceRequests\".\"officeSupplyRequest\"";
     // queries
-    String query = "INSERT INTO " + table + " (requester, roomName, supplies, additionalNotes, status, eta) VALUES (?,?,?,?,?,?) RETURNING requestID;";
+    String query =
+        "INSERT INTO "
+            + table
+            + " (requester, roomName, supplies, additionalNotes, status, eta) VALUES (?,?,?,?,?,?) RETURNING requestID;";
     try {
       ResultSet rs = db.getConnection().prepareStatement(query).executeQuery();
       while (rs.next()) {
         int requestID = rs.getInt("requestID");
-        request = new OfficeSuppliesRequest(requestID, orm.getRequester(), orm.getRoomName(), orm.getSupplies(), orm.getAdditionalNotes());
+        request =
+            new OfficeSuppliesRequest(
+                requestID,
+                orm.getRequester(),
+                orm.getRoomName(),
+                orm.getSupplies(),
+                orm.getAdditionalNotes());
         request.setStatus(orm.getStatus());
         request.setEta(orm.getEta());
       }
@@ -67,7 +75,7 @@ public class OfficeSuppliesRequestDAO {
     String query = "DELETE FROM " + table + " WHERE requestID=? ;";
     try {
       ResultSet rs = db.getConnection().prepareStatement(query).executeQuery();
-     request = null;
+      request = null;
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -75,12 +83,15 @@ public class OfficeSuppliesRequestDAO {
     return request;
   }
 
-  public OfficeSuppliesRequest updateRow(OfficeSuppliesRequest orm, OfficeSuppliesRequest repl ) {
+  public OfficeSuppliesRequest updateRow(OfficeSuppliesRequest orm, OfficeSuppliesRequest repl) {
     DBConnection db = new DBConnection();
     OfficeSuppliesRequest request = null;
     String table = "\"ServiceRequests\".\"officeSupplyRequest\"";
     // queries
-    String query = "UPDATE " + table + " SET requester=?, roomName=?, supplies=?, additionalNotes=?, status =?, eta=? WHERE requestID=?;";
+    String query =
+        "UPDATE "
+            + table
+            + " SET requester=?, roomName=?, supplies=?, additionalNotes=?, status =?, eta=? WHERE requestID=?;";
     try {
       PreparedStatement ps = db.getConnection().prepareStatement(query);
       ps.setString(1, repl.getRequester().toString());

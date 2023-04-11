899bf2481d91c01e65097b885f14c385b5a9799c
BJSamurai <juxieanchao@126.com>
2023-04-10 20:47:11 -0400

    Better order for Screen, Spanish draft 1

--- src/main/java/edu/wpi/teamc/navigation/Screen.java
+++ src/main/java/edu/wpi/teamc/navigation/Screen.java
@@ -1,29 +1,31 @@
 package edu.wpi.teamc.navigation;
 
 public enum Screen {
+  //----------------NON-USING SCREENS----------------//
   ROOT("views/Root.fxml"),
   SERVICE_REQUEST("views/ServiceRequest.fxml"),
+  //----------------MAP & SIGNAGE----------------//
   SIGNAGE("views/SignagePage.fxml"),
   ELEVATOR_SIGNAGE("views/ElevatorSignage.fxml"),
-  HOME("views/Home.fxml"),
-
   FLOOR_PLAN("views/FloorPlan.fxml"),
-
+  MAP_HISTORY_PAGE("views/MapChangeHistory.fxml"),
+  PATHFINDING_PAGE("views/Pathfinding.fxml"),
+  EDIT_MAP("views/MapEditing.fxml"),
+  //----------------HOME & LOGIN----------------//
   LOGIN("views/LoginPage.fxml"),
+  HOME("views/Home.fxml"),
   GUEST_HOME("views/GuestHome.fxml"),
+  //----------------SERVICE REQUESTS----------------//
   MEAL("views/Meal.fxml"),
   MEAL_HISTORY("views/MealHistory.fxml"),
   CONFERENCE("views/Conference.fxml"),
   CONFERENCE_HISTORY("views/ConferenceHistory.fxml"),
-
   FLOWER("views/Flower.fxml"),
   FURNITURE("views/Furniture.fxml"),
   OFFICE_SUPPLY("views/OfficeSupply.fxml"),
+  //----------------BONUS----------------//
   CONGRATS_PAGE("views/congratsPage.fxml"),
-  MAP_HISTORY_PAGE("views/MapChangeHistory.fxml"),
-  PATHFINDING_PAGE("views/Pathfinding.fxml"),
-  HELP("views/Help.fxml"),
-  EDIT_MAP("views/MapEditing.fxml");
+  HELP("views/Help.fxml");
 
   private final String filename;
 

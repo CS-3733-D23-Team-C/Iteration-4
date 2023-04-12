0b27030b8138b991f960241d72b15eb8cd64efd2
C-beaus <cabeausoleil@wpi.edu>
2023-04-11 20:31:43 -0400

    Made the start of the pathfinding page

--- src/main/java/edu/wpi/teamc/controllers/english/PathFindingController.java
+++ src/main/java/edu/wpi/teamc/controllers/english/PathFindingController.java
@@ -1,57 +1,232 @@
 package edu.wpi.teamc.controllers.english;
 
-// import edu.wpi.teamc.map.Graph;
+import edu.wpi.teamc.Main;
+import edu.wpi.teamc.dao.map.Node;
+import edu.wpi.teamc.dao.map.NodeDao;
+import edu.wpi.teamc.mapHelpers.CoordinatePasser;
 import edu.wpi.teamc.navigation.Navigation;
 import edu.wpi.teamc.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javax.swing.*;
+import net.kurobako.gesturefx.GesturePane;
+import org.controlsfx.control.tableview2.FilteredTableView;
 
 public class PathFindingController {
-  // Graph graph = new Graph();
-  @FXML private MFXButton goHome;
-  @FXML private MFXButton submit;
-  @FXML private MFXButton clear;
-  @FXML private MFXTextField startNode;
-  @FXML private MFXTextField endNode;
+  public Group group;
+  public Image image =
+          new Image(Main.class.getResource("./views/Images/GroundFloor.png").toString());
 
-  @FXML private Label directionsbox;
+  /*
+  Notes:
+  * implement import and export buttons and get rid of navigation actions
+  * implement a submit button that submits all edits at once ****** DONE (just need database functions for finding node based on ID)
+      * maybe store edits and their indices in an array and iterate over that array after the submit
+      * button is pressed to update the move list from the database
+  * draw edges over map page for pathfinding (currently PathfindingIt1)
+      * implement A* on this map
+      * have list of nodes you can pick from?
+      * be able to select nodes using listeners?
+      node id and longname to modify and shortname
+      longname, shortname, nodeid, nodetype, xcoord, ycoord, floor, building
+   */
 
+  /** */
+  @FXML MFXButton backButton;
+
+  /** Method run when controller is initialized */
   @FXML
-  void getGoHome() {
-    goHome.setOnMouseClicked(event -> Navigation.navigate(Screen.ADMIN_HOME));
+  public void goHome() {
+    backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
   }
 
-  @FXML
-  void getClear(javafx.event.ActionEvent event) {
-    startNode.clear();
-    endNode.clear();
-    directionsbox.setText("");
-  }
+  @FXML private Button testButton;
+  @FXML private TextField inputBox;
+  @FXML private FilteredTableView<TableRow> historyTable;
+  @FXML private FilteredTableView<TableRow> edgeTable;
+  @FXML TableView<TableRow> otherTable;
+  @FXML TableColumn<TableRow, String> ColumnOne;
+  @FXML TableColumn<TableRow, String> ColumnTwo;
+  @FXML TableColumn<TableRow, String> ColumnThree;
+  @FXML TableColumn<TableRow, String> ColumnFour;
+  @FXML TableColumn<TableRow, String> ColumnFive;
+  @FXML TableColumn<TableRow, String> ColumnSix;
+  @FXML TableColumn<TableRow, String> ColumnSeven;
+  @FXML TableColumn<TableRow, String> ColumnEight;
+  @FXML TableColumn<TableRow, String> ColumnNine;
+  @FXML GesturePane mapGPane;
+  @FXML MFXButton FL1;
+  @FXML MFXButton FL2;
+  @FXML MFXButton FL3;
+  @FXML MFXButton FLG;
+  @FXML MFXButton FLB1;
+  @FXML MFXButton FLB2;
+  @FXML MFXButton floorButton;
+  Group mapNodes = new Group();
+  CoordinatePasser cordPasser = new CoordinatePasser();
 
-  //  @FXML
-  //  void getSubmit(javafx.event.ActionEvent event) {
-  //    String startNodeString = startNode.getText();
-  //    String endNodeString = endNode.getText();
-  //    List<String> directions = graph.stringDirectionsAStar(startNodeString, endNodeString);
-  //    String directionsString = "";
-  //
-  //    directionsString += directions.get(0);
-  //    for (String s : directions.subList(1, directions.size() - 1)) {
-  //      directionsString += "->" + s;
-  //    }
-  //
-  //    directionsString += "\nWeight: " + directions.get(directions.size() - 1);
-  //    directionsbox.setWrapText(true);
-  //    directionsbox.setMaxWidth(330);
-  //    directionsbox.setText(directionsString);
-  //  }
-  /** Method run when controller is initializes */
+  //  @FXML TableColumn<TableRow, String> ColumnOne1;
+  //  @FXML TableColumn<TableRow, String> ColumnTwo1;
+  //  @FXML TableColumn<TableRow, String> ColumnThree1;
+  //  @FXML TableView<TableRow> testTable;
+  ObservableList<TableRow> rows = FXCollections.observableArrayList();
+  ObservableList<TableRow> rowsEdge = FXCollections.observableArrayList();
+
+  @FXML private Button goHome;
+  int XCoord = 0;
+  int YCoord = 0;
+  String xCoord_temp = "";
+  String yCoord_temp = "";
+  String nodeID_temp = "";
+  String iD;
+  String building = "";
+  String floor = "G";
+  List<Node> n_toAdd = new ArrayList<Node>();
+  List<Node> n_toModify_newNode = new ArrayList<Node>();
+  List<String> n_toModify_oldID = new ArrayList<String>();
+  List<String> n_toRemove = new ArrayList<String>();
+  List<Node> oldNameToAdd = new ArrayList<Node>();
+  List<String> newNameToAdd = new ArrayList<String>();
+
+  List<Node> oldNameToModify = new ArrayList<Node>();
+  List<String> newNameToModify = new ArrayList<String>();
+  List<String> nameToRemove = new ArrayList<String>();
+
+  String sNameInput_temp;
+  String lNameInput_temp;
+
+  //  List<Node> databaseNodeList = new ArrayList<Node>();
+  //  List<Edge> databaseEdgeList = new ArrayList<Edge>();
+  //  List<LocationName> databaseLocationNameList = new ArrayList<LocationName>();
+  //  List<Move> databaseMoveList = new ArrayList<Move>();
+
+  /** Method run when controller is initialized */
   public void initialize() {
-    // graph.syncWithDB();
+
+    //    File file = new File();
+    Image image = new Image(Main.class.getResource("./views/Images/GroundFloor.png").toString());
+    ImageView imageView = new ImageView(image);
+    imageView.relocate(0, 0);
+    group.getChildren().add(imageView);
+    group.getChildren().add(mapNodes);
+    Pane pane = new Pane();
+    pane.setMinWidth(image.getWidth());
+    pane.setMaxWidth(image.getWidth());
+    pane.setMinHeight(image.getHeight());
+    pane.setMaxHeight(image.getHeight());
+    pane.relocate(0, 0);
+    group.getChildren().add(pane);
+
+    placeNodes("G");
+  }
+
+  public void changeFloor(ActionEvent event) {
+    floorButton = (MFXButton) event.getTarget();
+
+    if (Objects.equals(floorButton.getId(), "FL1")) {
+      image = new Image(Main.class.getResource("./views/Images/FirstFloor.png").toString());
+      floor = "1";
+    } else if (Objects.equals(floorButton.getId(), "FL2")) {
+      image = new Image(Main.class.getResource("./views/Images/SecondFloor.png").toString());
+      floor = "2";
+    } else if (Objects.equals(floorButton.getId(), "FL3")) {
+      image = new Image(Main.class.getResource("./views/Images/ThirdFloor.png").toString());
+      floor = "3";
+    } else if (Objects.equals(floorButton.getId(), "FLG")) {
+      image = new Image(Main.class.getResource("./views/Images/GroundFloor.png").toString());
+      floor = "G";
+    } else if (Objects.equals(floorButton.getId(), "FLB1")) {
+      image = new Image(Main.class.getResource("./views/Images/B1.png").toString());
+      floor = "L1";
+    } else if (Objects.equals(floorButton.getId(), "FLB2")) {
+      image = new Image(Main.class.getResource("./views/Images/B2.png").toString());
+      floor = "L2";
+    }
+    group.getChildren().removeAll();
+    group.getChildren().remove(mapNodes);
+    ImageView imageView = new ImageView(image);
+    imageView.relocate(0, 0);
+    mapNodes = new Group();
+    group.getChildren().add(imageView);
+    group.getChildren().add(mapNodes);
+    Pane pane = new Pane();
+    pane.setMinWidth(image.getWidth());
+    pane.setMaxWidth(image.getWidth());
+    pane.setMinHeight(image.getHeight());
+    pane.setMaxHeight(image.getHeight());
+    pane.relocate(0, 0);
+    group.getChildren().add(pane);
+    placeNodes(floor);
+  } // initialize end
+
+  public void placeNodes(String floor) {
+    NodeDao nodeDao = new NodeDao();
+    nodeDao
+            .fetchAllObjects()
+            .forEach(
+                    TBP_node -> {
+                      if (Objects.equals(TBP_node.getFloor(), floor)) {
+                        createMapNodes(TBP_node);
+                      }
+                    });
+    mapNodes.toFront();
+  }
+  //  public void placeNodes(String floor, Node node) {
+  //
+  //  }
+
+  public void createMapNodes(Node node) {
+    String shortname = new NodeDao().getShortName(node.getNodeID());
+    Circle newCircle = new Circle();
+    if (!shortname.equals("")) {
+      Tooltip nodeName = new Tooltip(shortname);
+      nodeName.setShowDelay(Duration.ZERO);
+      nodeName.setShowDuration(Duration.hours(2));
+      Tooltip.install(newCircle, nodeName);
+    }
+    newCircle.setRadius(10);
+    newCircle.setCenterX(node.getXCoord());
+    newCircle.setCenterY(node.getYCoord());
+    newCircle.setId(String.valueOf(node.getNodeID()));
+    newCircle.setStroke(Paint.valueOf("#000000"));
+    newCircle.setFill(Paint.valueOf("#000000"));
+    newCircle.setVisible(true);
+    mapNodes.getChildren().add(newCircle);
+  }
+
+  public void getGoHome(ActionEvent event) {
+    Navigation.navigate(Screen.HOME);
+  }
+
+  public void getPathfindingPage(ActionEvent event) {
+    Navigation.navigate(Screen.PATHFINDING_PAGE);
+  }
+
+  public String getText(ActionEvent actionEvent) {
+    String inputtedText;
+    inputtedText = inputBox.getText();
+    inputBox.clear();
+    return inputtedText;
   }
 
   @FXML
@@ -94,12 +269,12 @@ public class PathFindingController {
 
   @FXML
   void getLogOut(ActionEvent event) {
-    Navigation.navigate(Screen.HOME);
+    Navigation.navigate(Screen.LOGIN);
   }
 
   @FXML
   void getExit(ActionEvent event) {
-    // Navigation.navigate(Screen.EXIT_PAGE);
+    System.exit(0);
   }
 
   @FXML
@@ -107,7 +282,6 @@ public class PathFindingController {
 
   @FXML
   void getMapPage(ActionEvent event) {}
+  /////////////////////////////////////////////
 
-  @FXML
-  void getPathfindingPage(ActionEvent event) {}
 }

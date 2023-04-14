6850511110945f83b56b7a48d2521022cd080f1b
randyer <rbdyer3@gmail.com>
2023-04-14 19:36:32 -0400

    main fix

--- src/main/java/edu/wpi/teamc/Main.java
+++ src/main/java/edu/wpi/teamc/Main.java
@@ -1,8 +1,19 @@
 package edu.wpi.teamc;
 
+import edu.wpi.teamc.dao.map.ImportCSV;
+
 public class Main {
 
   public static void main(String[] args) {
-    CApp.launch(CApp.class, args);
+        CApp.launch(CApp.class, args);
+//    ImportCSV importCSV = new ImportCSV();
+//    boolean test =
+//        importCSV.importAllCSV(
+//            "src/main/resources/edu/wpi/teamc/Node.csv",
+//            "src/main/resources/edu/wpi/teamc/Edge.csv",
+//            "src/main/resources/edu/wpi/teamc/Move.csv",
+//            "src/main/resources/edu/wpi/teamc/LocationName.csv");
+//
+//    System.out.println(test);
   }
 }

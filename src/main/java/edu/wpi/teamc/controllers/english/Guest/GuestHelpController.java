d8ec7c629fdcf7405470c51986fafef804bbe43e
azzhang3 <azhang3@wpi.edu>
2023-04-18 20:06:02 -0400

    Bug fixes, UI improvement - Aaron

--- src/main/java/edu/wpi/teamc/controllers/english/Guest/GuestHelpController.java
+++ src/main/java/edu/wpi/teamc/controllers/english/Guest/GuestHelpController.java
@@ -8,28 +8,28 @@ import javafx.scene.text.Text;
 import javax.swing.text.html.ImageView;
 
 public class GuestHelpController {
-    private String filePath;
-    private Desktop desktop = Desktop.getDesktop();
-    // @FXML private AdminMenuController adminMenuController;
+  private String filePath;
+  private Desktop desktop = Desktop.getDesktop();
+  // @FXML private AdminMenuController adminMenuController;
 
-    @FXML private ImageView English_flag;
-    @FXML private ImageView Spanish_flag;
-    @FXML private Text AdminHome_Title;
-    @FXML private javafx.scene.control.Button logoutButton;
-    @FXML private Button exitButton;
-    @FXML private MenuButton ServiceRequest;
-    @FXML private MenuButton NavigationButton;
-    @FXML private MenuButton Settings;
-    @FXML private MenuButton Help;
-    @FXML private TextArea basicFunctions;
-    @FXML private TextArea otherFunctions;
-    @FXML private Text helpTitle;
+  @FXML private ImageView English_flag;
+  @FXML private ImageView Spanish_flag;
+  @FXML private Text AdminHome_Title;
+  @FXML private javafx.scene.control.Button logoutButton;
+  @FXML private Button exitButton;
+  @FXML private MenuButton ServiceRequest;
+  @FXML private MenuButton NavigationButton;
+  @FXML private MenuButton Settings;
+  @FXML private MenuButton Help;
+  @FXML private TextArea basicFunctions;
+  @FXML private TextArea otherFunctions;
+  @FXML private Text helpTitle;
 
-    @FXML
-    public void initialize() {
-        basicFunctions.setWrapText(true);
-        basicFunctions.setEditable(false);
-        otherFunctions.setWrapText(true);
-        otherFunctions.setEditable(false);
-    }
+  @FXML
+  public void initialize() {
+    basicFunctions.setWrapText(true);
+    basicFunctions.setEditable(false);
+    otherFunctions.setWrapText(true);
+    otherFunctions.setEditable(false);
+  }
 }

c8a90c0161e8ff3f4ee38b7b40088b18cd1d7a33
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-04-18 23:03:46 -0400

    adminuser

--- src/main/java/edu/wpi/teamc/dao/users/AdminUser.java
+++ src/main/java/edu/wpi/teamc/dao/users/AdminUser.java
@@ -1,15 +1,3 @@
 package edu.wpi.teamc.dao.users;
 
-public class AdminUser extends AbsUser {
-  AdminUser(int id, String name, String username) {
-    super(id, name, username);
-  }
-
-  public AdminUser(String name, String username) {
-    super(name, username);
-  }
-
-  public String toString() {
-    return super.getUsername();
-  }
-}
+public class AdminUser extends EmployeeUser {}

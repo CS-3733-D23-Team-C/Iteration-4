83cf342ca71dc5b351a4cf8f365b284d5889fa08
tyakovenko <taisia.yakovenko@gmail.com>
2023-04-22 14:37:33 -0400

    logs + API set up

--- src/main/java/edu/wpi/teamc/Main.java
+++ src/main/java/edu/wpi/teamc/Main.java
@@ -1,8 +1,23 @@
 package edu.wpi.teamc;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Main {
+  public static final Logger logger =
+      Logger.getLogger("main"); // need to copy this and then just use logger. commands
 
   public static void main(String[] args) {
+    try (FileInputStream fis = new FileInputStream("logging.properties")) {
+      LogManager.getLogManager().readConfiguration(fis);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    logger.info("Start application");
     CApp.launch(CApp.class, args);
 
     //    ImportCSV importCSV = new ImportCSV();

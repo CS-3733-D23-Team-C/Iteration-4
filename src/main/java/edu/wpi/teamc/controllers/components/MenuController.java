4de7ea5c7433dc40c90e437e36aa70517f4c9ce8
joeyc5565 <jscaproni@wpi.edu>
2023-04-28 12:22:25 -0400

    Making menu clock

--- src/main/java/edu/wpi/teamc/controllers/components/MenuController.java
+++ src/main/java/edu/wpi/teamc/controllers/components/MenuController.java
@@ -9,10 +9,11 @@ import edu.wpi.teamc.dao.displays.Alert;
 import edu.wpi.teamc.navigation.Navigation;
 import edu.wpi.teamc.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
-import javafx.animation.PauseTransition;
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -1435,5 +1436,16 @@ public class MenuController {
                 warningPopOutTransition.play();
               }
             });
+
+    Timeline clock =
+        new Timeline(
+            new KeyFrame(
+                Duration.ZERO,
+                e ->
+                    menuClock.setText(
+                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("hh\n  :\nmm\na")))),
+            new KeyFrame(Duration.seconds(1)));
+    clock.setCycleCount(Animation.INDEFINITE);
+    clock.play();
   }
 }

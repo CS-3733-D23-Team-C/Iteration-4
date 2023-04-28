b35a240b0113f8921e8b850a0dbd3e91268c5bb9
randyer <rbdyer3@gmail.com>
2023-04-27 21:55:59 -0400

    connection for aws added

--- src/main/java/edu/wpi/teamc/dao/DBConnection.java
+++ src/main/java/edu/wpi/teamc/dao/DBConnection.java
@@ -19,6 +19,20 @@ public class DBConnection {
     }
   }
 
+  public void getAWSConnection() {
+    try {
+      // Load the PostgreSQL JDBC driver
+      Class.forName("org.postgresql.Driver");
+      // Establish the connection
+      String url = "jdbc:postgresql://softeng-c.c7x33sljy2ow.us-east-2.rds.amazonaws.com/teamc";
+      String user = "teamc";
+      String password = "teamcteamcteamc";
+      connection = DriverManager.getConnection(url, user, password);
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
   public void closeConnection() {
     try {
       connection.close();

e97d6ee44509368583654de910c4da8d0651910d
Nicholas Ishigami <106282967+nishigami13@users.noreply.github.com>
2023-05-01 18:42:07 -0400

    pathfinding and csv bug fixes

--- src/main/java/edu/wpi/teamc/Main.java
+++ src/main/java/edu/wpi/teamc/Main.java
@@ -1,8 +1,5 @@
 package edu.wpi.teamc;
 
-import static java.lang.System.exit;
-
-import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Main {
@@ -14,22 +11,14 @@ public class Main {
     logger.info("Start application");
     CApp.launch(CApp.class, args);
 
-    Scanner scan = new Scanner(System.in);
-    while (true) {
-      String command = scan.nextLine();
-      if (command.equals("exit")) {
-        exit(0);
-        break;
-      }
-      if (command.equals("help")) {
-        System.out.println("Commands: ");
-        System.out.println("exit - exit the program");
-        System.out.println("help - display this help message");
-        System.out.println("test screensaver - test screensaver");
-      }
-      if (command.equals("test screensaver")) {
-        CApp.logoutPopUp();
-      }
-    }
+    //    ImportCSV importCSV = new ImportCSV();
+    //    boolean test =
+    //        importCSV.importMapCSV(
+    //            "src/main/resources/edu/wpi/teamc/Node.csv",
+    //            "src/main/resources/edu/wpi/teamc/Edge.csv",
+    //            "src/main/resources/edu/wpi/teamc/Move.csv",
+    //            "src/main/resources/edu/wpi/teamc/LocationName.csv");
+    //
+    //    System.out.println(test);
   }
 }
